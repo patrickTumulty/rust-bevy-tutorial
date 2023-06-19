bcaee3ed8ea01b3c37f183a64040f830fe2deff3
ptumulty <ptumulty1@gmail.com>
2023-06-18 20:13:49 -0700

    Episode 2

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,44 @@
-mod lesson1;
 
-use lesson1::*;
+use bevy::window::PrimaryWindow;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugin(PeoplePlugin)
+        .add_startup_system(spawn_player)
+        .add_startup_system(spawn_camera)
         .run()
+}
+
+
+pub fn spawn_camera(mut commands: Commands, 
+                    window_query: Query<&Window, With<PrimaryWindow>>) 
+{
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+#[derive(Component)]
+pub struct Player {}
+
+pub fn spawn_player(mut commands: Commands, 
+                    window_query: Query<&Window, With<PrimaryWindow>>, 
+                    asset_server: Res<AssetServer>) 
+{
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0), 
+            texture: asset_server.load("sprites/ball_blue_large.png"),
+            ..default()
+        },
+        Player {},
+    ));
 }
 
 
